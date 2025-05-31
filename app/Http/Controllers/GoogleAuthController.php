37d8139185f0f254ef5a64878062cdc0b4a1008b
Nour Shaheen <nour@email.com>
2025-05-31 23:50:26 +0300

    edit googleLogin

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Google_Client;
 use App\Models\Patient;
-use DateTime;
 
 class GoogleAuthController extends Controller
 {
@@ -29,20 +28,6 @@ class GoogleAuthController extends Controller
         $firstName = $nameParts[0];
         $lastName = isset($nameParts[1]) ? implode(' ', array_slice($nameParts, 1)) : '';
 
-        $accessToken = $client->getAccessToken();
-        $response = file_get_contents("https://people.googleapis.com/v1/people/me?personFields=genders,birthdays&access_token={$accessToken}");
-        $userData = json_decode($response, true);
-
-        $gender = isset($userData['genders'][0]['value']) ? $userData['genders'][0]['value'] : 'Unknown';
-
-        $age = null;
-        if (isset($userData['birthdays'][0]['date'])) {
-            $birthDate = "{$userData['birthdays'][0]['date']['year']}-{$userData['birthdays'][0]['date']['month']}-{$userData['birthdays'][0]['date']['day']}";
-            $birthDateObj = new DateTime($birthDate);
-            $today = new DateTime();
-            $age = $today->diff($birthDateObj)->y;
-        }
-
         $user = User::firstOrCreate(
             ['email' => $email],
             [
@@ -58,8 +43,6 @@ class GoogleAuthController extends Controller
             [
                 'first_name' => $firstName,
                 'last_name' => $lastName,
-                'age' => $age,
-                'gender' => $gender,
             ]
         );
 
