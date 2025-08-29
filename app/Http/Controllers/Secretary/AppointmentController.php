b7bb9ade5c475a4881b7b579580fef55fcbc7cfc
Nour Shaheen <nour@email.com>
2025-08-29 07:42:24 +0300

    add doctor photo and sign on seeder

--- app/Http/Controllers/Secretary/AppointmentController.php
+++ app/Http/Controllers/Secretary/AppointmentController.php
@@ -497,7 +497,7 @@ class AppointmentController extends Controller
 
     public function showAllDoctors(Request $request)
     {
-        $doctors = Doctor::select('id', 'photo', 'first_name', 'last_name', 'speciality', 'status', 'finalRate', 'clinic_id', 'average_visit_duration');
+        $doctors = Doctor::select('id', 'photo', 'first_name', 'last_name', 'speciality', 'status', 'finalRate', 'clinic_id', 'average_visit_duration', 'sign');
 
         $data = $this->paginateResponse($request, $doctors, 'Doctors');
 
